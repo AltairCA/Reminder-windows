5bb57f9e5f7d7a19cde30f393f33e965a2120ddc
Altair <alexvista1234@gmail.com>
2017-03-22 11:01:16 +0530

    initial setup

--- Reminder/Form1.Designer.cs
+++ Reminder/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(13, 24);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 259);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
---
+++ Reminder/Player/SoundPlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder.Player
+{
+    class SoundPlayerController
+    {
+        [DllImport("winmm.dll")]
+        public static extern int waveOutGetVolume(IntPtr hwo, out uint dwVolume);
+
+        [DllImport("winmm.dll")]
+        public static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+
+        System.Timers.Timer timer;
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer(Reminder.Properties.Resources.Air_Horn_SoundBible_com_964603082);
+        public SoundPlayerController(int minutes,int seconds)
+        {
+            double milisecods = (minutes * 60* 1000) + seconds* 1000;
+            timer = new System.Timers.Timer(milisecods);
+            timer.AutoReset = true;
+            timer.Elapsed += PlaySound;
+            
+        
+        }
+        private void PlaySound(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            player.PlayLooping();
+        }
+        public void stopTimer()
+        {
+            player.Stop();
+            timer.Stop();
+            
+            
+        }
+        public void stopAlram()
+        {
+            player.Stop();
+        }
+        public void startTimer()
+        {
+            timer.Start();
+        }
+        public void startTimer(int minutes,int seconds)
+        {
+            double milisecods = (minutes * 60 * 1000) + seconds * 1000;
+            timer.Interval = milisecods;
+            timer.Start();
+        }
+        public void setPlayerVolume(int value)
+        {
+            // Calculate the volume that's being set. BTW: this is a trackbar!
+            int NewVolume = ((ushort.MaxValue / 10) * value);
+            // Set the same volume for both the left and the right channels
+            uint NewVolumeAllChannels = (((uint)NewVolume & 0x0000ffff) | ((uint)NewVolume << 16));
+            // Set the volume
+            waveOutSetVolume(IntPtr.Zero, NewVolumeAllChannels);
+        }
+        public int getPlayerVolume()
+        {
+            uint CurrVol = 0;
+            // At this point, CurrVol gets assigned the volume
+            waveOutGetVolume(IntPtr.Zero, out CurrVol);
+            // Calculate the volume
+            ushort CalcVol = (ushort)(CurrVol & 0x0000ffff);
+            // Get the volume on a scale of 1 to 10 (to fit the trackbar)
+            return CalcVol / (ushort.MaxValue / 10);
+        }
+
+    }
+}
